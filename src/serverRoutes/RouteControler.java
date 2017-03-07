83990d97adf8d65ebf23ac7ecbddccea2e49960f
Borgella JM <jmborgella@hotmail.com>
2017-03-07 11:26:51 -0500

    Update RouteControler.java

--- src/serverRoutes/RouteControler.java
+++ src/serverRoutes/RouteControler.java
@@ -7,11 +7,11 @@ import server.Request;
  *
  * @author Borgella
  *
- * Implementation of the Strategy pattern,the super super class RouteControler
+ * Implementation of the Strategy pattern,the super class RouteControler
  * defines an object name URIType that will give its subclasses the capability 
  * to decide what type of URYType they will be at runtime. In this case they will
  * decide if yes or no the client's request for a resource is available. 
- * Each URIType object will implements the itMatches method is own way. 
+ * Each URIType object will implements the itMatches method their own way. 
  * 
  */
 public abstract class RouteControler implements Route{
